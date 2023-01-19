98569dadbfc66705bde5bb9d020ac0f33c991236
DCypher <103863997+DCy4@users.noreply.github.com>
2023-01-19 19:31:30 +0000

    combined both functions into one called caesar, which both encodes and decodes the message

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(t, s):
-  cipher_text = ""
-  for letter in t:
-    #variable to store index of letter
-    position = alphabet.index(letter)
-    #add shift to the index of the letter
-    new_position = position + s   
-    #similar to position, this finds the new index of letter 
-    new_letter = alphabet[new_position]
-    #append this new letter to string variable outside of loop
-    cipher_text += new_letter
-  print(f"The encoded text is {cipher_text}")
+#Combine the encrypt() and decrypt() functions into a single function called caesar(). 
 
-#Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
-encrypt(t = text,s = shift)
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  #instead of calc for + or - shift amount just * -1 on decode
+  if cipher_direction == "decode":
+      shift_amount *= -1
+  for letter in start_text:
+    position = alphabet.index(letter)
+    new_position = position + shift_amount
+    end_text += alphabet[new_position]
+  print(f"Here's the {direction}d result: {end_text}")
+
+#Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
+caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
