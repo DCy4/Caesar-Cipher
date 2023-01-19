6ffacfaae9ae3cec91618af2eec04e8de11e6c27
DCypher <103863997+DCy4@users.noreply.github.com>
2023-01-19 20:27:08 +0000

    included logo, symbol/number/space adaptation, shift out of bounds error fix, and loop to restart program or exit when done.

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-#Combine the encrypt() and decrypt() functions into a single function called caesar(). 
-
 def caesar(start_text, shift_amount, cipher_direction):
   end_text = ""
-  #instead of calc for + or - shift amount just * -1 on decode
   if cipher_direction == "decode":
-      shift_amount *= -1
-  for letter in start_text:
-    position = alphabet.index(letter)
-    new_position = position + shift_amount
-    end_text += alphabet[new_position]
-  print(f"Here's the {direction}d result: {end_text}")
+    shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+    #What happens if the user enters a number/symbol/space?
+    #Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
+    #e.g. start_text = "meet me at 3"
+    #end_text = "•••• •• •• 3"
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+  print(f"Here's the {cipher_direction}d result: {end_text} \n")
 
-#Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
-caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+#Import and print the logo from art.py when the program starts.
+from art import logo 
+print(logo)
+#Can you figure out a way to ask the user if they want to restart the cipher program?
+#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
+#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
+#Hint: Try creating a while loop that continues to execute the program if the user types 'yes'. 
+should_continue = True
+while should_continue:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  
+  #What if the user enters a shift that is greater than the number of letters in the alphabet?
+  shift = shift % 26
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'. \n")
+  if restart == "no":
+    should_continue = False
+    print("Goodbye")
